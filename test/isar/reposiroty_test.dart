4e5f6d04d3a527b004ee31fac0ec89a97df2d928
leshgun <nogalexey@gmail.com>
2023-07-01 11:53:37 +0200

    code review

--- test/isar/reposiroty_test.dart
+++ test/isar/reposiroty_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:isar/isar.dart';
-import 'package:simplify_the_task/data/repositories/isar_repository.dart';
+import 'package:simplify_the_task/data/repositories/isar/isar.dart';
 
 class MockIsarRepository extends Mock implements IsarRepository {}
 
@@ -11,23 +11,5 @@ void main() {
   setUp(() async {
     sut = MockIsarRepository();
     await Isar.initializeIsarCore(download: true);
-  });
-
-  group('Initial values', () {
-    test(
-      'Documents directory is null at start',
-      () {
-        final docDir = sut.docDir;
-        expect(docDir, null);
-      },
-    );
-
-    test(
-      'App directory is null at start',
-      () {
-        final appDir = sut.appDir;
-        expect(appDir, null);
-      },
-    );
   });
 }
--- test/yandex/repository_test.dart
+++ test/yandex/repository_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:simplify_the_task/data/repositories/yandex_repository.dart';
+import 'package:simplify_the_task/data/repositories/yandex/yandex.dart';
 
 void main() {
   late YandexRepository sut;
