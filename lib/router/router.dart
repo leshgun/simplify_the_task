de64bddc6d5c6f51dcaf768b27ea79198e5acb60
leshgun <nogalexey@gmail.com>
2023-07-05 22:53:13 +0200

    git bug fixes

--- lib/router/router.dart
+++
@@ -1,7 +0,0 @@
-import 'package:simplify_the_task/features/task/task_info_screen.dart';
-import 'package:simplify_the_task/features/task_list/task_list_screen.dart';
-
-final routes = {
-  '/task-list': (context) => const TaskListScreen(),
-  '/task-info': (context) => const TaskInfoScreen(),
-};
